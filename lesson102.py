948db1c2e099aa9e582f19764049d9f6f895e4dc
Mark Peters <mapeters101@earthlink.net>
2025-03-11 11:33:45 -0400

    Work on lesson102 from 2:30 to 4:50.

--- lesson102.py
+++ lesson102.py
@@ -5,7 +5,19 @@ except ZeroDivisionError as ex:
     print(f'Exception occurred: {type(ex)}, {ex}')
 
 print('Code continues running here...')
+print('-'*80)
 
+l = [1,2,3,4,5]
+while len(l) > 0:
+    print(l.pop())
+print('all done')
+print('-'*80)
+
+l = [1,2,3,4,5]
+while True:
+    print(l.pop())
+print('all done')
+print('-'*80)
 
 
 

29f426b2742ebbb9752438d84f97695a59c75afa
Mark Peters <mapeters101@earthlink.net>
2025-03-11 11:25:40 -0400

    Complete work on lesson100 from 8:00.

--- lesson100.py
+++ lesson100.py
@@ -11,16 +11,26 @@ print(str(ex))
 print('-'*80)
 
 #raise ex
-
+'''
 name = input('Enter name (5 chars min): ')
 if len(name) < 5:
     raise ValueError(f'{name} is not 5 characters or more...')
 
 print(f'Hello {name}!')
+'''
 print('-'*60)
 
+print(issubclass(KeyError, LookupError))
+print(issubclass(KeyError, Exception))
+print(issubclass(IndexError, LookupError))
 
-
+ex = KeyError('some message')
+print(ex)
+print(type(ex))
+print(isinstance(ex, KeyError))
+print(isinstance(ex, LookupError))
+print(isinstance(ex, Exception))
+print(isinstance(ex, IndexError))
 
 
 

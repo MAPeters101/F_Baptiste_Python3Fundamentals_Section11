b487810b9a0807ab6ec5e5190f511f6c653a4c84
Mark Peters <mapeters101@earthlink.net>
2025-03-11 16:17:42 -0400

    Work on lesson102 from 10:40 to 13:10.

--- lesson102.py
+++ lesson102.py
@@ -50,15 +50,26 @@ except Exception:
 print('code resumes here..')
 print('-'*80)
 
-l = (1,2,3,4,5)
-try:
-    while True:
-        print(l.pop())
-except IndexError:
-    # index error means list is now empty - expected behavior
-    print('All done - all elements have been popped.')
-print('code resumes here..')
-print('-'*80)
+# l = (1,2,3,4,5)
+# try:
+#     while True:
+#         print(l.pop())
+# except IndexError:
+#     # index error means list is now empty - expected behavior
+#     print('All done - all elements have been popped.')
+# print('code resumes here..')
+# print('-'*80)
+
+#10 + 'abc'
+#10 + None
+data = [10, 20, 10, 10, 5, 10]
+sum_data = 0
+count_data = 0
+for element in data:
+    sum_data += element
+    count_data += 1
+average = sum_data / count_data
+print(average)
 
 
 
